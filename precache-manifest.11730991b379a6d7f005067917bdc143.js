9a2f9383d658b8bbd134e68f629e0a4585863a94
BryanF1nes <bryanfinesw@gmail.com>
2023-07-23 12:56:32 -0400

    Updates

---
+++ precache-manifest.11730991b379a6d7f005067917bdc143.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f151dae570b956193363",
+    "url": "/personal-portfolio/static/css/main.f5f5b7f2.chunk.css"
+  },
+  {
+    "revision": "f151dae570b956193363",
+    "url": "/personal-portfolio/static/js/main.cbf16d23.chunk.js"
+  },
+  {
+    "revision": "3fdc1bf041fe5486d21e",
+    "url": "/personal-portfolio/static/js/runtime~main.52b99ba9.js"
+  },
+  {
+    "revision": "40f0562f75c36b94563b",
+    "url": "/personal-portfolio/static/js/2.ce6bbfa5.chunk.js"
+  },
+  {
+    "revision": "278bb1a1e59d10282b78",
+    "url": "/personal-portfolio/static/js/3.3f8aa67b.chunk.js"
+  },
+  {
+    "revision": "3abe000a6029c3e36d03b5a00c7c3b48",
+    "url": "/personal-portfolio/static/media/calculator.3abe000a.png"
+  },
+  {
+    "revision": "f80b41979e5eb6e4c810c1ab833b75d3",
+    "url": "/personal-portfolio/index.html"
+  }
+];
