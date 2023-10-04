4b821c93c82b9d9e8e55e5846d9d4f499efd39d8
Bryan <bryanfinesw@gmail.com>
2023-10-03 23:43:52 -0400

    Test Commit

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
       <Section darkMode={darkMode} />
       <Portfolio darkMode={darkMode} />
       <Contact darkMode={darkMode} />
+      <h1>Testing Stuff</h1>
     </div>
   );
 }
